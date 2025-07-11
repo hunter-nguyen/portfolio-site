9b1516270ab92e72efd6bc24b2e9d06caa58d8d6
Hunter Nguyen <hunternguyen8@gmail.com>
2025-07-10 18:02:01 -0700

    add timeline route and render posts

--- app/__init__.py
+++ app/__init__.py
@@ -29,6 +29,10 @@ mydb.create_tables([TimelinePost])
 def index():
     return render_template('index.html', title="MLH Fellow", url=os.getenv("URL"))
 
+@app.route('/timeline')
+def timeline():
+    return render_template('timeline.html', title="Timeline", url=os.getenv("URL"))
+
 @app.route('/api/timeline_post', methods=['POST'])
 def post_time_line_post():
     name = request.form['name']
