89504d7bd567758f02df76e34c93380b0915ae73
Hunter Nguyen <hunternguyen8@gmail.com>
2025-07-25 20:26:53 -0400

    add git fetch to redeploy-site

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 cd ~/portfolio-site
-git reset origin/main --hard
+git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
