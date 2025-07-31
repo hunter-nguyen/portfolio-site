7754c47433d848124bba27c6d1421096f7419d84
Baljinder Singh Hothi <baljinderh.cs@gmail.com>
2025-07-31 07:34:29 +0000

    Add redeploy script for Docker deployment

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-# Go to your project directory
 cd ~/portfolio-site || exit
-
-# Pull latest changes from GitHub
 git fetch && git reset origin/main --hard
 
-# Activate virtual environment and install any new requirements
-source venv/bin/activate
-pip install -r requirements.txt
-
-# Restart the systemd service
-sudo systemctl restart myportfolio
+docker compose -f docker-compose.prod.yml down
+docker compose -f docker-compose.prod.yml up -d --build
